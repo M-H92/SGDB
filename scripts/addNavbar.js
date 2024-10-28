3a2383e91281a8c5d18ab3582a34aa07136eab50
Marc-Henry Sana <mhsana@biaclog.lu>
2024-10-28 13:11:45 +0100

    test setup pour navigation

--- scripts/addNavbar.js
+++ scripts/addNavbar.js
@@ -1,15 +1,15 @@
 import { getNavbar } from "./navbar.js";
 
 const _linkDefinitions = [
-  { name: "Préambule", url: "/chapters/chapter_0.html" },
-  { name: "Première DB", url: "/chapters/chapter_1.html" },
-  { name: "Relations", url: "/chapters/chapter_2.html" },
-  { name: "Contraintes", url: "/chapters/chapter_3.html" },
-  // { name: "Chapitre 4", url: "/chapters/chapter_4.html" },
-  { name: "Cardinalité", url: "/chapters/chapter_5.html" },
-  { name: "Problème de dates", url: "/chapters/chapter_6.html" },
-  { name: "Propriétaires", url: "/chapters/chapter_7.html" },
-  { name: "Liens et références externes", url: "/chapters/chapter_x.html" },
+  { name: "Préambule", url: "/SGDB/chapters/0" },
+  { name: "Première DB", url: "/SGDB/chapters/1" },
+  { name: "Relations", url: "/SGDB/chapters/2" },
+  { name: "Contraintes", url: "/SGDB/chapters/3" },
+  // { name: "Chapitre 4", url: "/SGDB/chapters/4" },
+  { name: "Cardinalité", url: "/SGDB/chapters/5" },
+  { name: "Problème de dates", url: "/SGDB/chapters/6" },
+  { name: "Propriétaires", url: "/SGDB/chapters/7" },
+  { name: "Liens et références externes", url: "/SGDB/chapters/x" },
 ];
 
 function configure() {
